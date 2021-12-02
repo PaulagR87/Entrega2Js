51eb5f877315eaef08eaaf48b09005c6f8264a54
Paula Rodriguez <paula.g.rodriguez@gmail.com>
2021-12-02 02:06:50 -0300

    git

---
+++ js/entrega2.js
@@ -0,0 +1,11 @@
+var  NombreUsuario = prompt('introduce tu nombre: ');
+    alert('Bienvenido ' + NombreUsuario );
+
+var aceptar = prompt('cuantos gatitos quieres adoptar?');
+alert('Gracias por adoptar ' + aceptar );
+
+  var num1=parseInt(prompt('Monto de donacion 0 a 10 pesos'));
+  var num2=parseInt(prompt('Donacion entre 0 a 100 pesos'));
+  var suma = num2 + num1;
+  alert('suma total de donación ' + suma );
+
