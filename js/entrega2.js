da7cedb0cf6135fc202b689fef06501103e78a9b
Paula Rodriguez <paula.g.rodriguez@gmail.com>
2021-12-05 12:35:49 -0300

    edad

--- js/entrega2.js
+++ js/entrega2.js
@@ -4,8 +4,13 @@ var  NombreUsuario = prompt('introduce tu nombre: ');
 var aceptar = prompt('cuantos gatitos quieres adoptar?');
 alert('Gracias por adoptar ' + aceptar );
 
-  var num1=parseInt(prompt('Monto de donacion 0 a 10 pesos'));
-  var num2=parseInt(prompt('Donacion entre 0 a 100 pesos'));
-  var suma = num2 + num1;
-  alert('suma total de donación ' + suma );
+ var edad;
+ edad=prompt("ingrese su edad: ");
+ if(edad>=18){
+  alert("puede adoptar");
+ }
+
+else{
+  alert("necesita ser mayor de edad para adoptar")
+}
 
